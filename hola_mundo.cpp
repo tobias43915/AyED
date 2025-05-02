9f04369d5972b7d67d0bee8ed457d2dcf17fa2f2
Tobias Carballo <tocarballo@frba.utn.edu.ar>
2025-05-02 20:00:05 -0300

    añadido nombre T

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-int main(){
-
-    cout << "Hola Bruno Barco" << endl;
+int main()
+{
+    cout<< "Hola Tobias Carballo" <<endl;
+    cout<< "Hola Bruno Barco" <<endl;
     
     return 0;
 }
