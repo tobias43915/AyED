aac78c334067218d469397392cd857ccd3fdcf4e
mromeroboeris <mromeroboeris@frba.utn.edu.ar>
2025-05-08 18:41:56 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout<< "Hola Bruno Barco" <<endl;
     cout<< "Hola Celeste Vera" <<endl;
     cout << "Hola Andrea Coria"<<endl;
+    cout<< "Hola Mora Romero" <<endl;
     return 0;
 }
